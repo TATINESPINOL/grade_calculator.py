9380426f77ca4cf7d3077dc5566470878706335c
TATINESPINOL <justinespinol07@gmail.com>
2026-02-11 18:34:12 +0800

    Add files via upload
    add grade calculator with if else statements

---
+++ grade_checker.py
@@ -0,0 +1,12 @@
+score = int(input("Enter your test grade: "))
+
+if score >= 90:
+    print("Test grade: A - excellent")
+elif score >= 80:
+    print("Test grade: B - very good")
+elif score >= 70:
+    print("Test grade: C - good")
+elif score >= 60:
+    print("Test grade: D - satisfactory")
+else:
+    print("Test grade: F - buruboooo")
